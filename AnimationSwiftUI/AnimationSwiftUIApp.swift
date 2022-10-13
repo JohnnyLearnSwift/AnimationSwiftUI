2bbb67df5d0f907621e9c8f289fe0441611d5199
JohnyLearnSwift <eugenietyan@gmail.com>
2022-10-13 20:48:43 +0300

    Initial Commit

---
+++ AnimationSwiftUI/AnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSwiftUIApp.swift
+//  AnimationSwiftUI
+//
+//  Created by Eugenie Tyan on 13.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
